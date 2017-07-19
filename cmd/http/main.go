65c85a4fdda5b847a7baecb8c401273685b72890
mk <zhu.johnny@mkcorp.com>
2017-07-19 17:22:18 +0800

    update git command

--- cmd/http/main.go
+++ cmd/http/main.go
@@ -9,18 +9,26 @@ import (
 )
 
 func Get(url string) {
-	url+="?sort=desc"
-	url+="&room=xxx"
-	url+="&offset=0"
-	url+="&max=10"
+	//url+="?sort=desc"
+	//url+="&room=xxx"
+	//url+="&offset=0"
+	//url+="&max=10"
 	fmt.Println(url)
+
+	http.Header{}.Add("User-Agent", "Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1 wechatdevtools/6.5.6 MicroMessenger/{{version}} Language/zh_CN webview/0")
+
 	resp, err :=http.Get(url)
 	if err != nil {
 		// handle error
 		fmt.Println(err)
 	}
 	defer resp.Body.Close()
+
 	body, err := ioutil.ReadAll(resp.Body)
+	if err !=nil{
+		fmt.Println(err)
+	}
+
 	fmt.Println(string(body))
 }
 
@@ -54,11 +62,10 @@ func MessageFetch(url string) {
 }
 
 func main() {
-	var url="";
-	MessageFetch(url)
+	var url=""
+	Get(url)
 	fmt.Println("test")
 }
-
 
 type Message struct {
 	Room          string `json:"room"`
@@ -90,6 +97,37 @@ func Post(at,url string) {
 		// handle error
 		fmt.Println(err)
 	}
+
+	fmt.Println(string(resBody))
+}
+
+func PostWechat(url string) {
+	msg:=&Message{
+		Room:"",
+		Type:257,
+		Msg:"对的，是这样的",
+		Extra:"{\"name\":\"xxx 024\"}",
+	}
+	body, _ := json.Marshal(msg)
+	req, err := http.NewRequest("GET", url, strings.NewReader(string(body)))
+	//req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("User-Agent", "Mozilla/5.0 (Linux; U; Android 2.3.6; zh-cn; GT-S5660 Build/GINGERBREAD) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 Mobile Safari/533.1 MicroMessenger/4.5.255")
+	//req.Header.Add("Referer", "https://mp.weixin.qq.com")
+	if err != nil{
+
+		fmt.Println(err)
+	}
+	resp, err := http.DefaultClient.Do(req)
+
+	defer resp.Body.Close()
+
+	resBody, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle error
+		fmt.Println(err)
+	}
+
+	fmt.Println("complete")
 
 	fmt.Println(string(resBody))
 }
--- git-command.md
+++ git-command.md
@@ -1,2 +1,67 @@
 this is a git command tutorial
 
+基础概念：
+
+repository  仓储
+
+client repository :  HEAD
+
+remote repository : origin
+
+
+branch 分支
+
+相关名称：
+
+// 创建一个名称为branch1的分支
+git branch branch1  
+
+//切换到branch1分支
+git checkout branch1
+
+
+commit  一次新的提交
+
+githash   会产生对应的一次唯一的提交标示
+
+相关命令：
+
+git status
+
+git diff
+
+git add
+
+git commit -a -m
+
+git push
+
+代码的回滚:
+
+git reset  githash   回滚到指定的commit，显示变更的文件
+
+git reset --hard  githash   回滚到指定的commit ,不显示变更的文件
+
+
+如果需要回滚到指定的commit并且提交的话：
+
+git branch branch1BackUp  先针对这个分支做备份
+
+git reset --hard  githash   回滚到指定的commit ,不显示变更的文件
+ 
+git push -f  强制覆盖remote端代码
+
+tag   标签，可以为某一个具体的commit打一个标记, 为以后可能会对当前版本做比较或者启用做记录
+
+创建tag
+git tag -a v1.0.1 36e0061849ec7ceeaf55f3a22ee3d49d7ad08aaa  -m 'add tag for current commit'
+
+git push origin v1.0.1
+
+删除tag
+
+git tag -d tagname 
+
+merge  代码的合并
+
+
